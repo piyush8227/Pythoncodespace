9e34a6516fcf87ad79ffe4f573cce8329acc5dcc
piyushmore8227 <piyushmore8227@gmail.com>
2023-03-09 12:17:18 +0530

    Day 12 Assignment.

--- Basics/mathematics.py
+++ Basics/mathematics.py
@@ -1,5 +1,10 @@
+# # print("Module")
+# print(__name__)
 def square(n):
     return n**2
 
-def sqrt(n):
-    return n**(1/2)
+# Dual behaviour in python.
+if __name__ == '__main__':
+    print(square(9))
+# def sqrt(n):
+#     return n**(1/2)
