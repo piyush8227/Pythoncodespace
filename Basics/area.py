943ff52e13fe0832a7d8804b792487be54631ef4
piyushmore8227 <piyushmore8227@gmail.com>
2023-03-16 12:33:52 +0530

    Short Notes on ML topics

---
+++ Basics/area.py
@@ -0,0 +1,12 @@
+
+def area(width, height):
+    result = width * height
+    return result
+
+def square(side):
+    result = side**2
+    return result
+
+def rectangle(length, breadth):
+    result = length * breadth
+    return result
